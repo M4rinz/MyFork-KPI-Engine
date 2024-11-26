f4a559352340e52592cf70c04fdee158ea5f0b77
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-11-26 19:47:34 +0100

    Exceptions file

---
+++ src/app/kpi_engine/exceptions.py
@@ -0,0 +1,22 @@
+class EmptyQueryException(Exception):
+    def __init__(self, message=None):
+        if message is None:
+            message = "The query returned an empty dataframe"
+        super().__init__(message)
+
+
+class InvalidFormulaReferenceException(Exception):
+    def __init__(self, message="The formula is not present in the KB"):
+        super().__init__(message)
+
+
+class InvalidBinaryOperatorException(Exception):
+    def __init__(
+        self, message="A binary operator is not present in the list of operations"
+    ):
+        super().__init__(message)
+
+
+class InvalidKPINameException(Exception):
+    def __init__(self, message="Invalid KPI name"):
+        super().__init__(message)
