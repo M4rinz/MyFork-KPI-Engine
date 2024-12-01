da833b0198cf6723c47c8622a95d374d90bbd36f
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-12-01 19:00:09 +0100

    Fixed bug in the docker-compose and added mount script for running the application.

---
+++ mount.sh
@@ -0,0 +1,8 @@
+cd ..
+
+git clone https://github.com/Kreative-Performative-Individuals/KB.git
+git clone https://github.com/Kreative-Performative-Individuals/smart-industrial-database.git
+
+cd KPI-Engine || exit 1
+
+docker-compose up --build
