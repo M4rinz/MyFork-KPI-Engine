bef39d6cd1d17efd91a08942e109f93025e3e8b8
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-12-17 00:29:17 +0100

    Formatting and added WebSocketDisconnect exception handling

--- src/app/kpi_engine/dynamic/dynamic_engine.py
+++ src/app/kpi_engine/dynamic/dynamic_engine.py
@@ -162,8 +162,6 @@ def query_DB(kpi: str, request: KPIRequest, **kwargs) -> tuple[np.ndarray, np.nd
             numpy_data.shape[0] // step, step, numpy_data.shape[1]
         )
 
-    
-
     return step_split, bottom
 
 
@@ -340,12 +338,11 @@ def compute(request: KPIRequest, chart: bool) -> KPIResponse:
     )
 
     if not chart:
-        _= insert_aggregated_kpi(
+        _ = insert_aggregated_kpi(
             request=request,
             kpi_list=formulas.keys(),
             value=result,
         )
-        
 
     return KPIResponse(message=message, value=result)
 
@@ -392,7 +389,7 @@ def keys_involved(kpi: str, partial_result: dict[str, Any]):
 def check_machine_operation(machines, operations):
     if isinstance(machines, str):
         try:
-            machines='"'+machines+'"'
+            machines = '"' + machines + '"'
             machine = get_closest_instances(machines)["instances"]
         except Exception as e:
             return KPIResponse(message=repr(e), value=-1)
--- src/app/services/database.py
+++ src/app/services/database.py
@@ -9,22 +9,22 @@ def insert_aggregated_kpi(
     kpi_list: list,
     value: np.float64,
 ):
-    
+
     data = {
-        'name': request.name,
-        'aggregated_value' : value.item(),
-        'begin_datetime' : str(request.start_date),
-        'end_datetime' : str(request.end_date),
-        'kpi_list' : list(kpi_list),
-        'operations' : request.operations,
-        'machines' : request.machines,
-        'step' : request.step,
+        "name": request.name,
+        "aggregated_value": value.item(),
+        "begin_datetime": str(request.start_date),
+        "end_datetime": str(request.end_date),
+        "kpi_list": list(kpi_list),
+        "operations": request.operations,
+        "machines": request.machines,
+        "step": request.step,
     }
 
     print("Inserting aggregated KPI data into the database...", data)
 
     return requests.post(
         "http://smart-database-container:8002/insert_aggregated_kpi",
-        json = data,
+        json=data,
         timeout=5,
     )
