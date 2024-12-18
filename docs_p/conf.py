71d1a023dec381d1659f0afdaee5fcb84cde4207
FabrizioDeCastelli <decastellifabrizio@gmail.com>
2024-12-18 19:22:04 +0100

    Formatting

--- docs_p/conf.py
+++ docs_p/conf.py
@@ -8,24 +8,24 @@
 
 import os
 import sys
+
 sys.path.insert(0, os.path.abspath("/home/mariiai7/Desktop/SmartApp/KPI-Engine/src"))
 
-project = 'KPI Engine'
-copyright = '2024, Maria Immacolata Colella, Fabrizio De Castelli, Marco Minniti, Filippo Alessandro Sandoval Villarreal'
-author = 'Maria Immacolata Colella, Fabrizio De Castelli, Marco Minniti, Filippo Alessandro Sandoval Villarreal'
+project = "KPI Engine"
+copyright = "2024, Maria Immacolata Colella, Fabrizio De Castelli, Marco Minniti, Filippo Alessandro Sandoval Villarreal"
+author = "Maria Immacolata Colella, Fabrizio De Castelli, Marco Minniti, Filippo Alessandro Sandoval Villarreal"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
